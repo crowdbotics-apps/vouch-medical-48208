bb01b4c6a645340fe7b9a6b78ef27af5df01b0b1
Crowdbotics <team@crowdbotics.com>
2024-05-31 19:00:22 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "vouch-medical-48208",
+  url: "https://vouch-medical-48208.botics.co",
+  api: "https://vouch-medical-48208.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
